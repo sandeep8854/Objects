c8160ec84ad2a8530262453500c315d1623493a7
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2024-01-09 23:35:17 +0530

    adding first program

---
+++ products.js
@@ -0,0 +1,74 @@
+const product = [
+  {
+    shampoo: {
+      price: "$50",
+      quantity: 4,
+    },
+    HairOil: {
+      price: "$40",
+      quantity: 2,
+      sealed: true,
+    },
+    comb: {
+      price: "$12",
+      quantity: 1,
+    },
+    utensils: [
+      {
+        spoons: { quantity: 2, price: "$8" },
+      },
+      {
+        glasses: { quantity: 1, price: "$70", type: "fragile" },
+      },
+      {
+        cooker: { quantity: 4, price: "$900" },
+      },
+    ],
+    watch: {
+      price: "$800",
+      quantity: 1,
+      type: "fragile",
+    },
+  },
+];
+
+// Q1. Find all the items with price more than $65.
+// Q2. Find all the items where quantity ordered is more than 1.
+// Q.3 Get all items which are mentioned as fragile.
+
+function findEniqu(product) {
+  // step -1
+  //   let ex = Object.keys(product[0]);
+  //   console.log(ex);
+
+  const expensiveItems = Object.keys(product[0]).reduce((acc, category) => {
+    const item = product[0][category];
+    // console.log(item);
+    if (Array.isArray(item)) {
+      item.forEach((subItem) => {
+        // console.log(subItem);
+        const subItemKey = Object.keys(subItem)[0];
+        const subItemValue = subItem[subItemKey];
+        if (subItemValue.price && parseInt(subItemValue.price.slice(1)) > 65) {
+          acc.push({ [subItemKey]: subItemValue });
+        }
+      });
+    } else {
+      if (item.price && parseInt(item.price.slice(1)) > 65) {
+        acc.push({ [category]: item });
+      }
+    }
+    return acc;
+  }, []);
+  return expensiveItems;
+}
+console.log(findEniqu(product));
+
+/*
+Output:- 
+[
+  { glasses: { quantity: 1, price: '$70', type: 'fragile' } },
+  { cooker: { quantity: 4, price: '$900' } },
+  { watch: { price: '$800', quantity: 1, type: 'fragile' } }
+]
+*/
