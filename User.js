0e2b85b2765f08746ae813f693eb4df0c2fc2013
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2024-01-11 13:36:40 +0530

    adding some file

---
+++ User.js
@@ -0,0 +1,73 @@
+const users = {
+  John: {
+    age: 24,
+    desgination: "Senior Golang Developer",
+    interests: ["Chess, Reading Comics, Playing Video Games"],
+    qualification: "Masters",
+    nationality: "Greenland",
+  },
+  Ron: {
+    age: 19,
+    desgination: "Intern - Golang",
+    interests: ["Video Games"],
+    qualification: "Bachelor",
+    nationality: "UK",
+  },
+  Wanda: {
+    age: 24,
+    desgination: "Intern - Javascript",
+    interests: ["Piano"],
+    qualification: "Bachaelor",
+    nationality: "Germany",
+  },
+  Rob: {
+    age: 34,
+    desgination: "Senior Javascript Developer",
+    interest: ["Walking his dog, Cooking"],
+    qualification: "Masters",
+    nationality: "USA",
+  },
+  Pike: {
+    age: 23,
+    desgination: "Python Developer",
+    interests: ["Listing Songs, Watching Movies"],
+    qualification: "Bachaelor's Degree",
+    nationality: "Germany",
+  },
+};
+
+// Q1 Find all users who are interested in playing video games.
+
+function playingVideoGame(users) {
+  const keyResultData = Object.keys(users).reduce((acc, ele) => {
+    // console.log(ele);
+    const nameOfUserValue = users[ele]; //we have key so we can access the value.
+    //  console.log(nameOfPlayer);
+    let tempArr = nameOfUserValue.interests;
+    // console.log(tempArr);
+    if (tempArr !== undefined && tempArr[0].includes("Playing Video Games")) {
+      acc.push(users[ele]);
+    }
+    return acc;
+  }, []);
+  return keyResultData;
+}
+//console.log(playingVideoGame(users));
+/*
+[
+  {
+    age: 24,
+    desgination: 'Senior Golang Developer',
+    interests: [ 'Chess, Reading Comics, Playing Video Games' ],
+    qualification: 'Masters',
+    nationality: 'Greenland'
+  }
+]
+*/
+//=============================================================================
+// Q2 Find all users staying in Germany.
+// Q3 Sort users based on their seniority level
+//  for Designation - Senior Developer > Developer > Intern
+//  for Age - 20 > 10
+//  Q4 Find all users with masters Degree.
+// Q5 Group users based on their Programming language mentioned in their designation.
