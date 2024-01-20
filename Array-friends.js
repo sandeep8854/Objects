14dd55e343e4361ebb6c8ed590f070e837d2856a
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2024-01-20 20:04:31 +0530

    Adding new question

--- Array-friends.js
+++ Array-friends.js
@@ -1,283 +1,260 @@
 let info = [
-    {
-        "_id": "61c0a7aee1b34fd049e7ecff",
-        "index": 0,
-        "guid": "c9bd4844-11ba-4f83-9056-a928b25f4b6c",
-        "isActive": true,
-        "balance": "$2,513.91",
-        "picture": "http://placehold.it/32x32",
-        "age": 36,
-        "eyeColor": "brown",
-        "name": "Stella Gordon",
-        "gender": "female",
-        "company": "TECHMANIA",
-        "email": "stellagordon@techmania.com",
-        "phone": "+1 (880) 418-2225",
-        "address": "439 Conduit Boulevard, Cressey, South Dakota, 9306",
-        "about": "Amet in cillum laborum ut eu adipisicing id tempor cillum qui. Culpa adipisicing duis fugiat sunt nisi sint fugiat est ad magna mollit ad minim. Dolore magna id et veniam. Nulla nisi officia sit ipsum ea. Dolore velit occaecat aliqua cillum aliquip irure proident consectetur id aliqua dolor. Magna minim ut eu pariatur excepteur esse consectetur quis ullamco mollit esse incididunt proident dolor. Mollit culpa consequat aute ullamco non Lorem reprehenderit in dolor excepteur sit.\r\n",
-        "registered": "2014-09-26T06:30:35 -06:-30",
-        "latitude": -0.821496,
-        "longitude": 105.413846,
-        "tags": [
-            "ut",
-            "exercitation",
-            "consequat",
-            "laborum",
-            "ut",
-            "incididunt",
-            "voluptate"
-        ],
-        "friends": [
-            {
-                "id": 0,
-                "name": "Marylou Randall"
-            },
-            {
-                "id": 1,
-                "name": "Case Daniels"
-            },
-            {
-                "id": 2,
-                "name": "Edwina White"
-            }
-        ],
-        "greeting": "Hello, Stella Gordon! You have 2 unread messages.",
-        "favoriteFruit": "apple"
-    },
-    {
-        "_id": "61c0a7ae1fb22a06905558f4",
-        "index": 1,
-        "guid": "5e84a583-6cae-4919-af8d-1ef2b7b859c9",
-        "isActive": false,
-        "balance": "$3,245.80",
-        "picture": "http://placehold.it/32x32",
-        "age": 33,
-        "eyeColor": "blue",
-        "name": "Warren Franks",
-        "gender": "male",
-        "company": "COMDOM",
-        "email": "warrenfranks@comdom.com",
-        "phone": "+1 (840) 563-3941",
-        "address": "495 Overbaugh Place, Hailesboro, Georgia, 3392",
-        "about": "Laborum voluptate eu laborum eiusmod laborum ut sunt sint reprehenderit sit. Id duis commodo consectetur do commodo esse duis voluptate irure consectetur pariatur labore proident velit. In sit enim ad non velit fugiat aliquip dolore amet veniam ullamco. Irure consequat esse incididunt labore et eu ad fugiat ut minim. Nisi aute in ipsum aliqua elit esse ipsum ea amet minim. Aliquip ad mollit exercitation aliqua deserunt eu irure.\r\n",
-        "registered": "2020-03-10T09:38:59 -06:-30",
-        "latitude": 75.346441,
-        "longitude": 156.412215,
-        "tags": [
-            "irure",
-            "dolor",
-            "ipsum",
-            "anim",
-            "anim",
-            "est",
-            "velit"
-        ],
-        "friends": [
-            {
-                "id": 0,
-                "name": "Tammie Preston"
-            },
-            {
-                "id": 1,
-                "name": "Perry Richard"
-            },
-            {
-                "id": 2,
-                "name": "Tyson Barton"
-            }
-        ],
-        "greeting": "Hello, Warren Franks! You have 2 unread messages.",
-        "favoriteFruit": "banana"
-    },
-    {
-        "_id": "61c0a7ae9383d9136d9bb2c0",
-        "index": 2,
-        "guid": "94141255-87bb-4f5a-a1b9-954e488325a5",
-        "isActive": false,
-        "balance": "$1,994.31",
-        "picture": "http://placehold.it/32x32",
-        "age": 31,
-        "eyeColor": "blue",
-        "name": "Jan Powell",
-        "gender": "female",
-        "company": "AVIT",
-        "email": "janpowell@avit.com",
-        "phone": "+1 (827) 578-3425",
-        "address": "821 Main Street, Zortman, Marshall Islands, 7908",
-        "about": "Dolore aliqua est sit tempor cupidatat ea enim mollit voluptate sunt ex culpa ipsum mollit. Magna sit proident labore officia deserunt dolore Lorem velit aliqua nulla nulla voluptate. Tempor in eiusmod esse excepteur nisi est proident quis proident in ut aliqua. Duis fugiat excepteur esse occaecat officia excepteur sunt aute ex proident. Laborum pariatur ipsum nulla mollit ex incididunt dolore eiusmod ex.\r\n",
-        "registered": "2019-11-24T06:49:33 -06:-30",
-        "latitude": 19.769582,
-        "longitude": 13.020114,
-        "tags": [
-            "elit",
-            "adipisicing",
-            "ad",
-            "eu",
-            "commodo",
-            "consequat",
-            "velit"
-        ],
-        "friends": [
-            {
-                "id": 0,
-                "name": "Burgess Valencia"
-            },
-            {
-                "id": 1,
-                "name": "Harmon Dorsey"
-            },
-            {
-                "id": 2,
-                "name": "Melody Buck"
-            }
-        ],
-        "greeting": "Hello, Jan Powell! You have 5 unread messages.",
-        "favoriteFruit": "strawberry"
-    },
-    {
-        "_id": "61c0a7ae7c828d2efb7cb66d",
-        "index": 3,
-        "guid": "ca1d88d2-86b0-4276-9b76-1f3868d23ef2",
-        "isActive": false,
-        "balance": "$1,043.03",
-        "picture": "http://placehold.it/32x32",
-        "age": 21,
-        "eyeColor": "brown",
-        "name": "Althea Lucas",
-        "gender": "female",
-        "company": "ACCUPRINT",
-        "email": "althealucas@accuprint.com",
-        "phone": "+1 (908) 419-2172",
-        "address": "303 Willow Place, Lopezo, Rhode Island, 6628",
-        "about": "Voluptate aliqua labore amet nostrud proident dolore occaecat duis. Proident proident dolore sunt eu cupidatat qui incididunt veniam nostrud sit qui. Id duis ipsum id cillum sunt nostrud labore id ipsum tempor occaecat irure.\r\n",
-        "registered": "2020-11-25T07:33:13 -06:-30",
-        "latitude": -67.189757,
-        "longitude": -177.645452,
-        "tags": [
-            "dolore",
-            "excepteur",
-            "occaecat",
-            "irure",
-            "exercitation",
-            "sit",
-            "ea"
-        ],
-        "friends": [
-            {
-                "id": 0,
-                "name": "Kidd Pitts"
-            },
-            {
-                "id": 1,
-                "name": "Deborah Pace"
-            },
-            {
-                "id": 2,
-                "name": "Gonzalez Sandoval"
-            }
-        ],
-        "greeting": "Hello, Althea Lucas! You have 9 unread messages.",
-        "favoriteFruit": "strawberry"
-    },
-    {
-        "_id": "61c0a7ae5ca4d4b6f61d29d3",
-        "index": 4,
-        "guid": "4c5af54f-e219-40a2-a17c-174ecaa9ca34",
-        "isActive": false,
-        "balance": "$1,546.07",
-        "picture": "http://placehold.it/32x32",
-        "age": 22,
-        "eyeColor": "blue",
-        "name": "Bartlett Chaney",
-        "gender": "male",
-        "company": "EXOBLUE",
-        "email": "bartlettchaney@exoblue.com",
-        "phone": "+1 (953) 592-3577",
-        "address": "350 Hampton Place, Bordelonville, Iowa, 7792",
-        "about": "Est cupidatat dolor dolore veniam in elit amet. Cupidatat ad sunt magna amet ipsum nostrud adipisicing sit id duis esse adipisicing ipsum. Amet ea adipisicing qui sint veniam fugiat voluptate id sit ad et aliquip sint nostrud. Ut do ut incididunt reprehenderit amet velit excepteur Lorem. Anim non labore voluptate ut duis labore cillum occaecat labore do ipsum labore incididunt dolor.\r\n",
-        "registered": "2014-10-19T01:13:08 -06:-30",
-        "latitude": 11.372088,
-        "longitude": 147.034209,
-        "tags": [
-            "mollit",
-            "nisi",
-            "amet",
-            "esse",
-            "deserunt",
-            "esse",
-            "proident"
-        ],
-        "friends": [
-            {
-                "id": 0,
-                "name": "Marcie Morrow"
-            },
-            {
-                "id": 1,
-                "name": "Lambert Myers"
-            },
-            {
-                "id": 2,
-                "name": "Marian Hunt"
-            }
-        ],
-        "greeting": "Hello, Bartlett Chaney! You have 7 unread messages.",
-        "favoriteFruit": "strawberry"
-    },
-    {
-        "_id": "61c0a7ae7c56687140f14a1e",
-        "index": 5,
-        "guid": "48f3cdd8-a45c-4c25-8170-7944b4dccf14",
-        "isActive": true,
-        "balance": "$3,506.50a",
-        "picture": "http://placehold.it/32x32",
-        "age": 29,
-        "eyeColor": "blue",
-        "name": "Fannie Baldwin",
-        "gender": "female",
-        "company": "OPTICOM",
-        "email": "fanniebaldwin@opticom.com",
-        "phone": "+1 (978) 578-2517",
-        "address": "544 Nautilus Avenue, Felt, Delaware, 7471",
-        "about": "Consectetur culpa nulla adipisicing Lorem eu exercitation consequat amet mollit elit cillum. Est in dolore fugiat non. Proident tempor et sit nostrud dolore ad. Ut occaecat exercitation anim cupidatat quis in. Excepteur voluptate dolor enim aliqua.\r\n",
-        "registered": "2018-06-24T08:47:27 -06:-30",
-        "latitude": -52.037284,
-        "longitude": 38.050724,
-        "tags": [
-            "ea",
-            "exercitation",
-            "sit",
-            "aute",
-            "elit",
-            "nulla",
-            "fugiat"
-        ],
-        "friends": [
-            {
-                "id": 0,
-                "name": "Sharpe Guthrie"
-            },
-            {
-                "id": 1,
-                "name": "Dionne Bright"
-            },
-            {
-                "id": 2,
-                "name": "Viola Mcfarland"
-            }
-        ],
-        "greeting": "Hello, Fannie Baldwin! You have 2 unread messages.",
-        "favoriteFruit": "banana"
-    }
-]
-/* 
-  1. Make the balance into a number. If the number is invalid, default to 0. Do not overwrite the 
-  
-  
-  existing key, create a new one instead.
-  2. Sort by descending order of age.
-  3. Flatten the friends key into a basic array of names.
-  4. Remove the tags key.
-  5. Make the registered date into the DD/MM/YYYY format.
-  6. Filter only the active users.
-  7. Calculate the total balance available.
+  {
+    _id: "61c0a7aee1b34fd049e7ecff",
+    index: 0,
+    guid: "c9bd4844-11ba-4f83-9056-a928b25f4b6c",
+    isActive: true,
+    balance: "$2,513.91",
+    picture: "http://placehold.it/32x32",
+    age: 36,
+    eyeColor: "brown",
+    name: "Stella Gordon",
+    gender: "female",
+    company: "TECHMANIA",
+    email: "stellagordon@techmania.com",
+    phone: "+1 (880) 418-2225",
+    address: "439 Conduit Boulevard, Cressey, South Dakota, 9306",
+    about:
+      "Amet in cillum laborum ut eu adipisicing id tempor cillum qui. Culpa adipisicing duis fugiat sunt nisi sint fugiat est ad magna mollit ad minim. Dolore magna id et veniam. Nulla nisi officia sit ipsum ea. Dolore velit occaecat aliqua cillum aliquip irure proident consectetur id aliqua dolor. Magna minim ut eu pariatur excepteur esse consectetur quis ullamco mollit esse incididunt proident dolor. Mollit culpa consequat aute ullamco non Lorem reprehenderit in dolor excepteur sit.\r\n",
+    registered: "2014-09-26T06:30:35 -06:-30",
+    latitude: -0.821496,
+    longitude: 105.413846,
+    tags: [
+      "ut",
+      "exercitation",
+      "consequat",
+      "laborum",
+      "ut",
+      "incididunt",
+      "voluptate",
+    ],
+    friends: [
+      {
+        id: 0,
+        name: "Marylou Randall",
+      },
+      {
+        id: 1,
+        name: "Case Daniels",
+      },
+      {
+        id: 2,
+        name: "Edwina White",
+      },
+    ],
+    greeting: "Hello, Stella Gordon! You have 2 unread messages.",
+    favoriteFruit: "apple",
+  },
+  {
+    _id: "61c0a7ae1fb22a06905558f4",
+    index: 1,
+    guid: "5e84a583-6cae-4919-af8d-1ef2b7b859c9",
+    isActive: false,
+    balance: "$3,245.80",
+    picture: "http://placehold.it/32x32",
+    age: 33,
+    eyeColor: "blue",
+    name: "Warren Franks",
+    gender: "male",
+    company: "COMDOM",
+    email: "warrenfranks@comdom.com",
+    phone: "+1 (840) 563-3941",
+    address: "495 Overbaugh Place, Hailesboro, Georgia, 3392",
+    about:
+      "Laborum voluptate eu laborum eiusmod laborum ut sunt sint reprehenderit sit. Id duis commodo consectetur do commodo esse duis voluptate irure consectetur pariatur labore proident velit. In sit enim ad non velit fugiat aliquip dolore amet veniam ullamco. Irure consequat esse incididunt labore et eu ad fugiat ut minim. Nisi aute in ipsum aliqua elit esse ipsum ea amet minim. Aliquip ad mollit exercitation aliqua deserunt eu irure.\r\n",
+    registered: "2020-03-10T09:38:59 -06:-30",
+    latitude: 75.346441,
+    longitude: 156.412215,
+    tags: ["irure", "dolor", "ipsum", "anim", "anim", "est", "velit"],
+    friends: [
+      {
+        id: 0,
+        name: "Tammie Preston",
+      },
+      {
+        id: 1,
+        name: "Perry Richard",
+      },
+      {
+        id: 2,
+        name: "Tyson Barton",
+      },
+    ],
+    greeting: "Hello, Warren Franks! You have 2 unread messages.",
+    favoriteFruit: "banana",
+  },
+  {
+    _id: "61c0a7ae9383d9136d9bb2c0",
+    index: 2,
+    guid: "94141255-87bb-4f5a-a1b9-954e488325a5",
+    isActive: false,
+    balance: "$1,994.31",
+    picture: "http://placehold.it/32x32",
+    age: 31,
+    eyeColor: "blue",
+    name: "Jan Powell",
+    gender: "female",
+    company: "AVIT",
+    email: "janpowell@avit.com",
+    phone: "+1 (827) 578-3425",
+    address: "821 Main Street, Zortman, Marshall Islands, 7908",
+    about:
+      "Dolore aliqua est sit tempor cupidatat ea enim mollit voluptate sunt ex culpa ipsum mollit. Magna sit proident labore officia deserunt dolore Lorem velit aliqua nulla nulla voluptate. Tempor in eiusmod esse excepteur nisi est proident quis proident in ut aliqua. Duis fugiat excepteur esse occaecat officia excepteur sunt aute ex proident. Laborum pariatur ipsum nulla mollit ex incididunt dolore eiusmod ex.\r\n",
+    registered: "2019-11-24T06:49:33 -06:-30",
+    latitude: 19.769582,
+    longitude: 13.020114,
+    tags: ["elit", "adipisicing", "ad", "eu", "commodo", "consequat", "velit"],
+    friends: [
+      {
+        id: 0,
+        name: "Burgess Valencia",
+      },
+      {
+        id: 1,
+        name: "Harmon Dorsey",
+      },
+      {
+        id: 2,
+        name: "Melody Buck",
+      },
+    ],
+    greeting: "Hello, Jan Powell! You have 5 unread messages.",
+    favoriteFruit: "strawberry",
+  },
+  {
+    _id: "61c0a7ae7c828d2efb7cb66d",
+    index: 3,
+    guid: "ca1d88d2-86b0-4276-9b76-1f3868d23ef2",
+    isActive: false,
+    balance: "$1,043.03",
+    picture: "http://placehold.it/32x32",
+    age: 21,
+    eyeColor: "brown",
+    name: "Althea Lucas",
+    gender: "female",
+    company: "ACCUPRINT",
+    email: "althealucas@accuprint.com",
+    phone: "+1 (908) 419-2172",
+    address: "303 Willow Place, Lopezo, Rhode Island, 6628",
+    about:
+      "Voluptate aliqua labore amet nostrud proident dolore occaecat duis. Proident proident dolore sunt eu cupidatat qui incididunt veniam nostrud sit qui. Id duis ipsum id cillum sunt nostrud labore id ipsum tempor occaecat irure.\r\n",
+    registered: "2020-11-25T07:33:13 -06:-30",
+    latitude: -67.189757,
+    longitude: -177.645452,
+    tags: [
+      "dolore",
+      "excepteur",
+      "occaecat",
+      "irure",
+      "exercitation",
+      "sit",
+      "ea",
+    ],
+    friends: [
+      {
+        id: 0,
+        name: "Kidd Pitts",
+      },
+      {
+        id: 1,
+        name: "Deborah Pace",
+      },
+      {
+        id: 2,
+        name: "Gonzalez Sandoval",
+      },
+    ],
+    greeting: "Hello, Althea Lucas! You have 9 unread messages.",
+    favoriteFruit: "strawberry",
+  },
+  {
+    _id: "61c0a7ae5ca4d4b6f61d29d3",
+    index: 4,
+    guid: "4c5af54f-e219-40a2-a17c-174ecaa9ca34",
+    isActive: false,
+    balance: "$1,546.07",
+    picture: "http://placehold.it/32x32",
+    age: 22,
+    eyeColor: "blue",
+    name: "Bartlett Chaney",
+    gender: "male",
+    company: "EXOBLUE",
+    email: "bartlettchaney@exoblue.com",
+    phone: "+1 (953) 592-3577",
+    address: "350 Hampton Place, Bordelonville, Iowa, 7792",
+    about:
+      "Est cupidatat dolor dolore veniam in elit amet. Cupidatat ad sunt magna amet ipsum nostrud adipisicing sit id duis esse adipisicing ipsum. Amet ea adipisicing qui sint veniam fugiat voluptate id sit ad et aliquip sint nostrud. Ut do ut incididunt reprehenderit amet velit excepteur Lorem. Anim non labore voluptate ut duis labore cillum occaecat labore do ipsum labore incididunt dolor.\r\n",
+    registered: "2014-10-19T01:13:08 -06:-30",
+    latitude: 11.372088,
+    longitude: 147.034209,
+    tags: ["mollit", "nisi", "amet", "esse", "deserunt", "esse", "proident"],
+    friends: [
+      {
+        id: 0,
+        name: "Marcie Morrow",
+      },
+      {
+        id: 1,
+        name: "Lambert Myers",
+      },
+      {
+        id: 2,
+        name: "Marian Hunt",
+      },
+    ],
+    greeting: "Hello, Bartlett Chaney! You have 7 unread messages.",
+    favoriteFruit: "strawberry",
+  },
+  {
+    _id: "61c0a7ae7c56687140f14a1e",
+    index: 5,
+    guid: "48f3cdd8-a45c-4c25-8170-7944b4dccf14",
+    isActive: true,
+    balance: "$3,506.50a",
+    picture: "http://placehold.it/32x32",
+    age: 29,
+    eyeColor: "blue",
+    name: "Fannie Baldwin",
+    gender: "female",
+    company: "OPTICOM",
+    email: "fanniebaldwin@opticom.com",
+    phone: "+1 (978) 578-2517",
+    address: "544 Nautilus Avenue, Felt, Delaware, 7471",
+    about:
+      "Consectetur culpa nulla adipisicing Lorem eu exercitation consequat amet mollit elit cillum. Est in dolore fugiat non. Proident tempor et sit nostrud dolore ad. Ut occaecat exercitation anim cupidatat quis in. Excepteur voluptate dolor enim aliqua.\r\n",
+    registered: "2018-06-24T08:47:27 -06:-30",
+    latitude: -52.037284,
+    longitude: 38.050724,
+    tags: ["ea", "exercitation", "sit", "aute", "elit", "nulla", "fugiat"],
+    friends: [
+      {
+        id: 0,
+        name: "Sharpe Guthrie",
+      },
+      {
+        id: 1,
+        name: "Dionne Bright",
+      },
+      {
+        id: 2,
+        name: "Viola Mcfarland",
+      },
+    ],
+    greeting: "Hello, Fannie Baldwin! You have 2 unread messages.",
+    favoriteFruit: "banana",
+  },
+];
+
+// 1. Make the balance into a number. If the number is invalid, default to 0. Do not overwrite the   existing key, create a new one instead.
+
+function makeABalFunc(info) {
+  console.log(info);
+}
+console.log(makeABalFunc(info));
+
+//  2. Sort by descending order of age.
+// 3. Flatten the friends key into a basic array of names.
+//  4. Remove the tags key.
+// 5. Make the registered date into the DD/MM/YYYY format.
+// 6. Filter only the active users.
+//  7. Calculate the total balance available.
